5a2446e54bbd4442767cb2cb10fade66ac351de6
Gyuhub <alsrb0820@gmail.com>
2023-01-15 00:30:56 +0900

    update path of dl_scratch package

--- dl_scratch/common/__init__.py
+++ dl_scratch/common/__init__.py
@@ -1,7 +1,7 @@
 """
 Package that includes common modules and functions for practice of deep learning
 """
-from common.functions import *
+from dl_scratch.common.functions import *
 import matplotlib.pylab as plt
 
 __ver__ = '0.1'
--- dl_scratch/common/functions.py
+++ dl_scratch/common/functions.py
@@ -9,3 +9,12 @@ def sigmoid(x):
 
 def relu(x):
     return np.maximum(x, 0)
+
+def identity(x):
+    return x
+
+def softmax(x):
+    x_max = np.max(x)
+    x_exp = np.exp(x - x_max)
+    x_exp_sum = np.sum(x_exp)
+    return x_exp / x_exp_sum
