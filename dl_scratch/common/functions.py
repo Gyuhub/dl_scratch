591684f4e188f8c49f3b1c8fdf9a7af90d3399a1
Mingyu Park <m1n9yu@kaist.ac.kr>
2023-02-08 15:45:19 +0900

    add loss functions

--- dl_scratch/common/functions.py
+++ dl_scratch/common/functions.py
@@ -18,3 +18,14 @@ def softmax(x):
     x_exp = np.exp(x - x_max)
     x_exp_sum = np.sum(x_exp)
     return x_exp / x_exp_sum
+
+def sum_squares_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+def cross_entropy_error(y, t):
+    if y.dim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
