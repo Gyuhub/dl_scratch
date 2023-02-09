7683bcc695748c45ef5b151a43d3bccfba0c8ae2
Mingyu Park <m1n9yu@kaist.ac.kr>
2023-02-09 19:54:25 +0900

    add a new module

--- dl_scratch/common/__init__.py
+++ dl_scratch/common/__init__.py
@@ -2,6 +2,7 @@
 Package that includes common modules and functions for practice of deep learning
 """
 from dl_scratch.common.functions import *
+from dl_scratch.common.gradient import *
 import matplotlib.pylab as plt
 
 __ver__ = '0.1'
