7966c34b133786feae5ea4555fbf75bf1cdd95eb
Mingyu Park <m1n9yu@kaist.ac.kr>
2023-02-23 19:47:20 +0900

    add a batch normalization layer

--- dl_scratch/common/layers.py
+++ dl_scratch/common/layers.py
@@ -95,3 +95,83 @@ class SoftmaxWithLossLayer:
         batch_size = self.t.shape[0]
         dx = (self.y - self.t) / batch_size
         return dx
+
+class BatchNormLayer:
+     def __init__(self, gamma, beta, momentum=0.9, rolling_mean=None, rolling_var=None):
+          self.gamma = gamma
+          self.beta = beta
+          self.momentum = momentum
+          self.input_shape = None # 4-D for convolution layer, 2-D for affine layer
+
+          # used when the network is run for testing, not learning
+          self.rolling_mean = rolling_mean
+          self.rolling_var = rolling_var
+
+          # used when backpropagation
+          self.batch_size = None
+          self.xd = None # deviation between data and mean
+          self.std = None # standarad deviation
+          self.xhat = None # normalized data
+          self.dgamma = None
+          self.dbeta = None
+
+     def forward(self, x, train_flag=True):
+          self.input_shape = x.shape
+          if x.ndim != 2:
+               N, C, H, W = x.shape # batch, channel, height, width
+               x = x.reshape(N, -1)
+          
+          out = self.__forward(x,train_flag)
+          return out.reshape(*self.input_shape)
+
+     def __forward(self, x, train_flag):
+          if self.rolling_mean == None:
+               N, D = x.shape
+               self.rolling_mean = np.zeros(D)
+               self.rolling_var = np.zeros(D)
+
+          if train_flag:
+               mu = np.mean(x, axis=0)
+               xd = x - mu
+               var = np.mean(xd**2, axis=0)
+               std = np.sqrt(var + 10e-7)
+               xhat = xd / std
+
+               self.batch_size = x.shape[0]
+               self.xd = xd
+               self.std = std
+               self.xhat = xhat
+
+               # Exponential Moving Average (EMA)
+               self.rolling_mean = self.momentum * self.rolling_mean (1 - self.momentum) * mu
+               self.rolling_var = self.momentum * self.rolling_var (1 - self.momentum) * var
+          else:
+               xd = x - self.rolling_mean
+               xhat = xd / (np.sqrt(self.running_var + 10e-7))
+          
+          out = self.gamma * xhat + self.beta
+          return out
+
+     def backward(self, dout):
+          if dout.ndim != 2:
+               N, C, H, W = dout.shape # batch, channel, height, width
+               dout = dout.reshape(N, -1)
+          
+          dx = self.__backward(dout)
+          dx = dx.reshape(*self.input_shape)
+          return dx
+
+     def __backward(self, dout):
+          dbeta = np.sum(dout, axis=0)
+          dgamma = np.sum(dout * self.xhat, axis=0)
+          dxhat = self.gamma * dout
+          dxd = dxhat / self.std
+          dstd = -np.sum((dxhat * self.xd) / (self.std**2), axis=0)
+          dvar = 0.5 * dstd / self.std
+          dxd += (2.0 / self.batch_size) * self.xd * dvar
+          dmu = np.sum(dxd, axis=0)
+          dx = dxd - dmu / self.batch_size
+
+          self.dgamma = dgamma
+          self.dbeta = dbeta
+          return dx
